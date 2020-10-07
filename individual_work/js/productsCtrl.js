be21cf0ad222c9d118e2cc35b574bca652f7ebb7
Evgheni Panenco <evgheni.panenco@gmail.com>
2020-10-07 14:40:00 +0300

    Individual work in progress

---
+++ individual_work/js/productsCtrl.js
@@ -0,0 +1,22 @@
+app.controller('productsCtrl', function ($scope) {
+    $scope.smartphones = [
+        { "title": "Galaxy Note 20", "price": 18999, "image": "resources/images/products-photo/n980_galaxy_note_20_green_9a.webp"},
+        { "title": "Galaxy A51", "price": 5999, "image": "resources/images/products-photo/samsung_galaxy_a515_blue1.png" },
+        { "title": "Galaxy A71", "price": 8299, "image": "resources/images/products-photo/galaxy_a715_silver_1.png" },
+        { "title": "Galaxy M30s", "price": 4999, "image": "resources/images/products-photo/samsung_galaxy_m307_black1.png" },
+        { "title": "Galaxy S10+", "price": 14299, "image": "resources/images/products-photo/samsung_galaxy_g975_red1.png" },
+        { "title": "Galaxy J4", "price": 2499, "image": "resources/images/products-photo/samsung_galaxy_j400_black5_ageg-1y.webp" },
+        { "title": "Galaxy Note 20", "price": 18999, "image": "resources/images/products-photo/n980_galaxy_note_20_green_9a.webp" },
+        { "title": "Galaxy A51", "price": 5999, "image": "resources/images/products-photo/samsung_galaxy_a515_blue1.png" },
+        { "title": "Galaxy A71", "price": 8299, "image": "resources/images/products-photo/galaxy_a715_silver_1.png" },
+        { "title": "Galaxy M30s", "price": 4999, "image": "resources/images/products-photo/samsung_galaxy_m307_black1.png" },
+        { "title": "Galaxy S10+", "price": 14299, "image": "resources/images/products-photo/samsung_galaxy_g975_red1.png" },
+        { "title": "Galaxy J4", "price": 2499, "image": "resources/images/products-photo/samsung_galaxy_j400_black5_ageg-1y.webp" },
+        { "title": "Galaxy Note 20", "price": 18999, "image": "resources/images/products-photo/n980_galaxy_note_20_green_9a.webp" },
+        { "title": "Galaxy A51", "price": 5999, "image": "resources/images/products-photo/samsung_galaxy_a515_blue1.png" },
+        { "title": "Galaxy A71", "price": 8299, "image": "resources/images/products-photo/galaxy_a715_silver_1.png" },
+        { "title": "Galaxy M30s", "price": 4999, "image": "resources/images/products-photo/samsung_galaxy_m307_black1.png" },
+        { "title": "Galaxy S10+", "price": 14299, "image": "resources/images/products-photo/samsung_galaxy_g975_red1.png" },
+        { "title": "Galaxy J4", "price": 2499, "image": "resources/images/products-photo/samsung_galaxy_j400_black5_ageg-1y.webp" }
+    ];
+});
